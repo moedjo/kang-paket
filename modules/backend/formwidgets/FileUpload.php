612889d4415c72379fb9b2d1e798a366403e9296
DESKTOP-8R0UOBE\Desktop <donypramujo@gmail.com>
2022-02-03 09:46:00 +0700

    no message

--- modules/backend/formwidgets/FileUpload.php
+++ modules/backend/formwidgets/FileUpload.php
@@ -150,6 +150,7 @@ class FileUpload extends FormWidgetBase
             throw new ApplicationException('Maximum allowed size for uploaded files: ' . $maxPhpSetting);
         }
 
+        $this->vars['size'] = $this->formField->size;
         $this->vars['fileList'] = $fileList = $this->getFileList();
         $this->vars['singleFile'] = $fileList->first();
         $this->vars['displayMode'] = $this->getDisplayMode();
@@ -160,7 +161,6 @@ class FileUpload extends FormWidgetBase
         $this->vars['maxFilesize'] = $this->maxFilesize;
         $this->vars['maxFiles'] = $this->maxFiles;
         $this->vars['cssDimensions'] = $this->getCssDimensions();
-        $this->vars['cssBlockDimensions'] = $this->getCssDimensions('block');
         $this->vars['useCaption'] = $this->useCaption;
     }
 
@@ -242,10 +242,9 @@ class FileUpload extends FormWidgetBase
 
     /**
      * getCssDimensions for the uploaded image, uses auto where no dimension is provided
-     * @param string $mode
      * @return string
      */
-    protected function getCssDimensions($mode = null)
+    protected function getCssDimensions()
     {
         if (!$this->imageWidth && !$this->imageHeight) {
             return '';
@@ -253,23 +252,12 @@ class FileUpload extends FormWidgetBase
 
         $cssDimensions = '';
 
-        if ($mode == 'block') {
-            $cssDimensions .= $this->imageWidth
-                ? 'width: '.$this->imageWidth.'px;'
-                : 'width: '.$this->imageHeight.'px;';
-
-            $cssDimensions .= ($this->imageHeight)
-                ? 'max-height: '.$this->imageHeight.'px;'
-                : 'height: auto;';
+        if ($this->imageWidth && !$this->imageHeight) {
+            $cssDimensions .= 'width: '.$this->imageWidth.'px;';
         }
-        else {
-            $cssDimensions .= $this->imageWidth
-                ? 'width: '.$this->imageWidth.'px;'
-                : 'width: auto;';
 
-            $cssDimensions .= ($this->imageHeight)
-                ? 'max-height: '.$this->imageHeight.'px;'
-                : 'height: auto;';
+        if ($this->imageHeight && !$this->imageWidth) {
+            $cssDimensions .= 'height: '.$this->imageHeight.'px;';
         }
 
         return $cssDimensions;
